475ad1ab9f0215fbbbbd6c0a8f9f4b7d5a411955
cong_zhang <cong_zhang@idgcapital.com>
2020-04-04 21:20:06 +0800

    init of commit to github

---
+++ distribution-session/src/main/java/abc/springboot/study/distribution/session/DistributionSessionApplication.java
@@ -0,0 +1,13 @@
+package abc.springboot.study.distribution.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionSessionApplication.class, args);
+    }
+
+}
---
+++ distribution-session/src/test/java/abc/springboot/study/distribution/session/DistributionSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package abc.springboot.study.distribution.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
